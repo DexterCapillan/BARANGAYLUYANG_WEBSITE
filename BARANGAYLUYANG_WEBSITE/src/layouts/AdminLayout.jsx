63dae2d8f929530009e1095316a14c8ff1d5601d
DexterCapillan <146181493+DexterCapillan@users.noreply.github.com>
2026-04-27 14:00:22 +0800

    Update AdminLayout.jsx

--- BARANGAYLUYANG_WEBSITE/src/layouts/AdminLayout.jsx
+++ BARANGAYLUYANG_WEBSITE/src/layouts/AdminLayout.jsx
@@ -2,7 +2,7 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import Sidebar from "../components/common/Sidebar";
 import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/authContext";
 
 export default function AdminLayout() {
   const { user, logout } = useContext(AuthContext);
